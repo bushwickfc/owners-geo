ec442aa9487689a1d2aecf8eedb18520bee699fb
Darren Klein <darren.a.klein@gmail.com>
2018-07-27 08:56:30 -0400

    Add leading datestring to the output file.

--- write_data.py
+++ write_data.py
@@ -1,10 +1,11 @@
 # Format the list of GeoJSON data and write it to a file.
+import datetime
 
 def write(filename, geojson):
     file = open(filename, 'w+')
     file.write(geojson)
 
 def execute(geojson):
-    filename = 'owners.geojson'
+    filename = f'{datetime.datetime.today().strftime("%Y-%m-%d")}-owners.geojson'
     print(f'Writing to file {filename}')
     write(filename, geojson)
